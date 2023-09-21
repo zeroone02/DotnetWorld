6bbbf0f1f7643f6d482b7e09aee0878cb9948cb0
artur <zeroone02@mail.ru>
2023-09-21 19:51:50 +0300

    Закончил создание метода SendAsync в BaseService

--- DotnetWorld-Server/apps/public-web/DotnetWorld.WebService/src/DotnetWorld.WebService.Application/Base/BaseService.cs
+++ DotnetWorld-Server/apps/public-web/DotnetWorld.WebService/src/DotnetWorld.WebService.Application/Base/BaseService.cs
@@ -1,11 +1,77 @@
 ﻿using DotnetWorld.DDD;
 using DotnetWorld.WebService.Application.Contracts;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using static DotnetWorld.WebService.Domain.SD;
 
 namespace DotnetWorld.WebService.Application;
 public class BaseService : IBaseService
 {
-    public Task<ResponseDto>? SendAsync(RequestDto requestDto)
+    private readonly IHttpClientFactory _httpClientFactory;
+    public BaseService(IHttpClientFactory httpClientFactory)
     {
-        throw new NotImplementedException();
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<ResponseDto>? SendAsync(RequestDto requestDto)
+    {
+        try
+        {
+            HttpClient client = _httpClientFactory.CreateClient("DotnetWorld");
+            HttpRequestMessage message = new();
+
+            message.Headers.Add("Accept", "application/json");
+            //token
+            message.RequestUri = new Uri(requestDto.Url);
+            if (requestDto.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.
+                    SerializeObject(requestDto.Data),
+                    Encoding.UTF8, "application/json");
+            }
+
+            switch (requestDto.ApiType)
+            {
+                case ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                case ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                default:
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+
+            HttpResponseMessage? apiResponse = await client.SendAsync(message);
+
+            switch (apiResponse.StatusCode)
+            {
+                case HttpStatusCode.NotFound:
+                    return new() { IsSuccess = false, Message = "Not Found" };
+                case HttpStatusCode.Forbidden:
+                    return new() { IsSuccess = false, Message = "Access Denied" };
+                case HttpStatusCode.Unauthorized:
+                    return new() { IsSuccess = false, Message = "Unauthorized" };
+                case HttpStatusCode.InternalServerError:
+                    return new() { IsSuccess = false, Message = "Internal Server Error" };
+                default:
+                    var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                    var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                    return apiResponseDto;
+            }
+        }
+        catch (Exception ex)
+        {
+            var dto = new ResponseDto
+            {
+                Message = ex.Message.ToString(),
+                IsSuccess = false
+            };
+            return dto;
+        }
     }
 }
