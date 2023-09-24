b18e502c0797ae475c2638c1ada5ba22b07eebd2
artur <zeroone02@mail.ru>
2023-09-24 15:37:11 +0300

    WIP

---
+++ DotnetWorld-Server/apps/public-web/DotnetWorld.WebService/src/DotnetWorld.WebService/Controllers/CouponController.cs
@@ -0,0 +1,26 @@
+﻿using DotnetWorld.DDD;
+using DotnetWorld.WebService.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DotnetWorld.WebService.Controllers;
+public class CouponController : Controller
+{
+    private readonly ICouponService _couponService;
+
+    public CouponController(ICouponService couponService)
+    {
+        _couponService = couponService;
+    }
+
+    public async Task<IActionResult> CouponIndex()
+    {
+        List<CouponDto>? list = new List<CouponDto>();
+        ResponseDto? response = await _couponService.GetAllCouponsAsync();
+        if (response != null && response.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+        }
+        return View(list);
+    }
+}
