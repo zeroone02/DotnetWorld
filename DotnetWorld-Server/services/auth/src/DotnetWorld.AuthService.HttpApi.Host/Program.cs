5700d48ff83b9095271b5fe98f3b44e5c34dbb61
artur <zeroone02@mail.ru>
2023-09-25 20:39:58 +0300

    Построил базовую архитектуру проета

--- DotnetWorld-Server/services/auth/src/DotnetWorld.AuthService.HttpApi.Host/Program.cs
+++ DotnetWorld-Server/services/auth/src/DotnetWorld.AuthService.HttpApi.Host/Program.cs
@@ -1,25 +1,94 @@
-var builder = WebApplication.CreateBuilder(args);
+//using AutoMapper;
+//using DotnetWorld.Application;
+//using DotnetWorld.AuthService.Application.Contracts;
+//using DotnetWorld.AuthService.Application;
+//using DotnetWorld.DDD;
+//using Microsoft.EntityFrameworkCore;
+//using DotnetWorld.AuthService.Domain;
+//using DotnetWorld.AuthService.EntityFrameworkCore;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        //builder.Services.AddDbContext<IEfCoreDbContext, CouponServiceDbContext>(options =>
+        //{
+        //    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+        //});
+
+        ConfigureServices(builder.Services);
+        //builder.AddAppAuthetication();
+
+        var app = builder.Build();
+
+        var configuration = app.Services.GetRequiredService<IConfiguration>();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        ConfigureEntityFrameworkCore(services);
+        ConfigureApplicationServices(services);
+        ConfigureAuthentication(services);
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        #region addswaggerWithAuth
+        //    services.AddSwaggerGen(option =>
+        //    {
+        //        option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+        //        {
+        //            Name = "Authorization",
+        //            Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+        //            In = ParameterLocation.Header,
+        //            Type = SecuritySchemeType.ApiKey,
+        //            Scheme = "Bearer"
+        //        });
+        //        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        //{
+        //    {
+        //        new OpenApiSecurityScheme
+        //        {
+        //            Reference= new OpenApiReference
+        //            {
+        //                Type=ReferenceType.SecurityScheme,
+        //                Id=JwtBearerDefaults.AuthenticationScheme
+        //            }
+        //        }, new string[]{}
+        //    }
+        //});
+        //    });
+        #endregion
+    }
+    private static void ConfigureAuthentication(IServiceCollection services)
+    {
+    }
+    private static void ConfigureApplicationServices(IServiceCollection services)
+    {
+        //services.AddTransient<ICouponService, CouponService>();
+        //services.AddTransient<UnitOfWork>();
+
+        //var mapperConfig = new MapperConfiguration(mc =>
+        //{
+        //    mc.AddProfile(new DotnetWorldApplicationObjectMapper());
+        //});
+
+        //IMapper mapper = mapperConfig.CreateMapper();
+        //services.AddSingleton(mapper);
+    }
+    private static void ConfigureEntityFrameworkCore(IServiceCollection services)
+    {
+        //services.AddTransient<IRepository<Coupon, Guid>, Repository<Coupon, Guid>>();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
