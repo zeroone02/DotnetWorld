c5595800da89b04996f45eb05ad9dfe789f2aeb1
artur <zeroone02@mail.ru>
2023-09-27 17:47:15 +0300

    WIP

---
+++ DotnetWorld-Server/services/auth/src/DotnetWorld.AuthService.HttpApi/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetWorld.AuthService.HttpApi;
+[Route("api/auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register()
+    {
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login()
+    {
+        return Ok();
+    }
+
+}
