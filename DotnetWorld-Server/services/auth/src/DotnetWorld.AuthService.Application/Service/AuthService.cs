4716e7545baebe35858dad9c906a574cf5991521
artur <zeroone02@mail.ru>
2023-09-28 19:39:40 +0300

    Добавил ApplicationUser в бд, зарегистрировал в DI контейнере новый  класс JwtOptions

---
+++ DotnetWorld-Server/services/auth/src/DotnetWorld.AuthService.Application/Service/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using DotnetWorld.AuthService.Application.Contracts;
+using DotnetWorld.AuthService.Domain;
+using eShop.AuthService.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace DotnetWorld.AuthService.Application;
+public class AuthService : IAuthService
+{
+    public readonly AuthServiceDbContext _db;
+    public readonly UserManager<ApplicationUser> _userManager;
+    public readonly RoleManager<IdentityRole> _roleManager;
+    public AuthService(AuthServiceDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _db = db;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LoginResponseDto> Register(LoginRequestDto loginRequestDto)
+    {
+        throw new NotImplementedException();
+    }
+}
--- DotnetWorld-Server/services/auth/src/DotnetWorld.AuthService.Domain/Models/ApplicationUser.cs
+++ DotnetWorld-Server/services/auth/src/DotnetWorld.AuthService.Domain/Models/ApplicationUser.cs
@@ -4,4 +4,5 @@ using Microsoft.AspNetCore.Identity;
 namespace DotnetWorld.AuthService.Domain;
 public class ApplicationUser : IdentityUser
 {
+    public string Name { get; set; }
 }
