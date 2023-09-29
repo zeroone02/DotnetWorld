6276d77ddbc1a578c722382637ebe0aba145528e
artur <zeroone02@mail.ru>
2023-09-29 20:52:07 +0300

    WIP

--- DotnetWorld-Server/services/auth/src/DotnetWorld.AuthService.Application/Service/AuthService.cs
+++ DotnetWorld-Server/services/auth/src/DotnetWorld.AuthService.Application/Service/AuthService.cs
@@ -16,13 +16,59 @@ public class AuthService : IAuthService
         _roleManager = roleManager;
     }
 
-    public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+    public async Task<string> RegisterAsync(RegistrationRequestDto registrationRequestDto)
     {
-        throw new NotImplementedException();
-    }
+        ApplicationUser user = new()
+        {
+            UserName = registrationRequestDto.Email,
+            Email = registrationRequestDto.Email,
+            NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+            Name = registrationRequestDto.Name,
+            PhoneNumber = registrationRequestDto.PhoneNumber
+        };
 
-    public Task<LoginResponseDto> Register(LoginRequestDto loginRequestDto)
+        try
+        {
+            var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+            if (result.Succeeded)
+            {
+                var userToReturn = _db.ApplicationUsers.First(u => u.UserName == registrationRequestDto.Email);
+                return "";
+            }
+            else
+            {
+                return result.Errors.FirstOrDefault().Description;
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+        return "Error Encountered";
+    }
+    public async Task<LoginResponseDto> LoginAsync(LoginRequestDto loginRequestDto)
     {
-        throw new NotImplementedException();
+       var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
+        bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+        if (user == null || isValid == false)
+        {
+            return new LoginResponseDto() { User = null, Token = "" };
+        }
+        var roles = await _userManager.GetRolesAsync(user);
+        //var token = _jwtTokenGenerator.GenerateToken(user, roles);
+        UserDto userDto = new()
+        {
+            Email = user.Email,
+            ID = user.Id,
+            Name = user.Name,
+            PhoneNumber = user.PhoneNumber
+        };
+        LoginResponseDto loginResponseDto = new()
+        {
+            User = userDto,
+            //Token = token
+        };
+        return loginResponseDto;
     }
 }
