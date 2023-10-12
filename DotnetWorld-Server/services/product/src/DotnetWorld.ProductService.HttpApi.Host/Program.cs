492a1456fe3efacbfb36931daf238790e39cb6f0
artur <zeroone02@mail.ru>
2023-10-12 17:43:35 +0300

    Начало создания ProductService

--- DotnetWorld-Server/services/product/src/DotnetWorld.ProductService.HttpApi.Host/Program.cs
+++ DotnetWorld-Server/services/product/src/DotnetWorld.ProductService.HttpApi.Host/Program.cs
@@ -1,25 +1,95 @@
-var builder = WebApplication.CreateBuilder(args);
+using AutoMapper;
+using DotnetWorld.Application;
+using DotnetWorld.CouponService.Application.Contracts;
+using DotnetWorld.CouponService.Application;
+using DotnetWorld.DDD;
+using Microsoft.EntityFrameworkCore;
+using DotnetWorld.CouponService.Domain;
+using DotnetWorld.CouponService.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<IEfCoreDbContext, ProductServiceDbContext>(options =>
+        {
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        ConfigureServices(builder.Services);
+        builder.AddAppAuthetication();
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        var configuration = app.Services.GetRequiredService<IConfiguration>();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        ConfigureEntityFrameworkCore(services);
+        ConfigureApplicationServices(services);
+        ConfigureAuthentication(services);
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(option =>
+        {
+            option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference= new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id=JwtBearerDefaults.AuthenticationScheme
+                    }
+                }, new string[]{}
+            }
+    });
+        });
+    }
+    private static void ConfigureAuthentication(IServiceCollection services)
+    {
+    }
+    private static void ConfigureApplicationServices(IServiceCollection services)
+    {
+        services.AddTransient<IProductService, ProductService>();
+        services.AddTransient<UnitOfWork>();
+
+        var mapperConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new DotnetWorldApplicationObjectMapper());
+        });
+
+        IMapper mapper = mapperConfig.CreateMapper();
+        services.AddSingleton(mapper);
+    }
+    private static void ConfigureEntityFrameworkCore(IServiceCollection services)
+    {
+        services.AddTransient<IRepository<Product, Guid>, Repository<Product, Guid>>();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
