da5ef0eac283b0c0c3567f870eb9c9d6ceea9bf9
artur <zeroone02@mail.ru>
2023-10-15 19:01:26 +0300

    WIP

---
+++ DotnetWorld-Server/services/shoppingCart/common/DotnetWorld.DDD.Application.Contracts/Dtos/EntityDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetWorld.DDD.Application.Contracts;
+public abstract class EntityDto
+{
+}
+
+public abstract class EntityDto<TKey> : EntityDto
+{
+    public TKey Id { get; set; }
+}
---
+++ DotnetWorld-Server/services/shoppingCart/common/DotnetWorld.DDD.Entity/Repositories/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetWorld.DDD;
+public interface IRepository<TEntity, TKey> 
+    : IReadOnlyRepository<TEntity, TKey>
+{
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}
